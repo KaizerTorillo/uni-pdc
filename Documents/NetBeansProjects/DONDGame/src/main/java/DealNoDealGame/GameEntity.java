21fe52d06994823284b1fc77b434b68160b2d33a
'Kaizer <kaizertorillo@gmail.com>
2025-06-10 16:29:49 +1200

    test commit to branch

---
+++ Documents/NetBeansProjects/DONDGame/src/main/java/DealNoDealGame/GameEntity.java
@@ -0,0 +1,20 @@
+
+package DealNoDealGame;
+
+/**
+ *
+ * @author Kaizer T
+ */
+public class GameEntity {
+    protected String name;
+    
+    public GameEntity(String name) 
+    {
+        this.name = name;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+}
